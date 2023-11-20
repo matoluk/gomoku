4c81358d4283237fbed23fd6a5d97b602af03a3d
Matej Lukacik <matej.lukacik@gmail.com>
2023-11-20 21:00:06 +0100

    Created random engine

---
+++ src/EngineRandom.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+
+public class EngineRandom implements Engine {
+    private Game game;
+    private Move bestMove;
+    private int[] board;
+    private final int cellBitSize = 2;
+    private final int empty = 0;
+    private final int myStone = 1;
+    private final int opponentStone = 2;
+    private final int mask = 3;
+    EngineRandom(Game game){
+        this.game = game;
+        board = new int[Settings.size];
+        for (int i = 0; i < Settings.size; i++)
+            board[i] = 0;
+    }
+    private int getCell(Move move){
+        return (board[move.x] & (mask << (move.y * cellBitSize)));
+    }
+    @Override
+    public void opponentMove(Move move) {
+        assert (getCell(move) == empty);
+        board[move.x] += opponentStone << (move.y * cellBitSize);
+    }
+
+    @Override
+    public void go(int time, int opponentTime, int moveTime) {
+        Random random = new Random();
+        Move move = new Move(random.nextInt(Settings.size), random.nextInt(Settings.size));
+        while (getCell(move) != empty)
+            move = new Move(random.nextInt(Settings.size), random.nextInt(Settings.size));
+        bestMove = move;
+    }
+
+    @Override
+    public void stop() {
+        game.bestMove(bestMove);
+    }
+
+    @Override
+    public void quit() {
+
+    }
+}
